cb36a41fc7ed54b4c784c38fa2cafb0140698b64
Dor-Ron <dor.rondel@gmail.com>
2015-08-07 22:31:38 -0400

    added contact modal

--- static/js/index.js
+++ static/js/index.js
@@ -9,3 +9,14 @@ $("#area").mouseenter(function() {
 $("#area").mouseleave(function() {
     $("#cursor").show();
 });
+
+var alert = {
+	body: '<div style="text-align:center"><h1 style="color:orange; font-family: \'Indie Flower\', cursive; font-size:40px">Contact Information</h1><br/><p style="color:orange; font-family: \'Indie Flower\', cursive; font-size: 25px;">You can e-mail me at<p style="font-size: 30px; color:orange; font-family: \'Indie Flower\', cursive;"> dor.rondel@gmail.com</p></p></div>',
+	buttons: {
+		action: {
+			title: 'Dismiss',
+			fn: basicModal.close
+		}
+	}
+}
+document.querySelector('a.alert').addEventListener('click', function() { basicModal.show(alert) })
---
+++ static/js/index.js~
@@ -0,0 +1,11 @@
+$(document).mousemove(function(e){
+		$("#cursor").css({left:e.pageX, top:e.pageY});
+});
+
+$("#area").mouseenter(function() {
+    $("#cursor").hide();
+});
+
+$("#area").mouseleave(function() {
+    $("#cursor").show();
+});
