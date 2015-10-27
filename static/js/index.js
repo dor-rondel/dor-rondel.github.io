2b94f0be12b65dfc1abb53223b23eaf080c6d60a
Dor-Ron <dor.rondel@gmail.com>
2015-10-27 16:05:59 -0400

    fixed contact/resume responsiveness issue

--- static/js/index.js
+++ static/js/index.js
@@ -10,6 +10,16 @@ $("#area").mouseleave(function() {
     $("#cursor").show();
 });
 
+$(document).ready(function () {
+	$(".small").hide();
+
+    if (screen.width < 1024) {
+        $(".big").hide();
+        $(".small").show();
+    }
+    
+});
+
 var alert = {
 	body: '<div style="text-align:center"><h1 style="color:orange; font-family: \'Indie Flower\', cursive; font-size:40px">Contact Information</h1><br/><p style="color:orange; font-family: \'Indie Flower\', cursive; font-size: 25px;">You can e-mail me at<p style="font-size: 30px; color:orange; font-family: \'Indie Flower\', cursive;"> dor.rondel@gmail.com</p></p></div>',
 	buttons: {
