6cca4eb5cb9e6208cc54ab2020cfe95d5af16d5e
Dor-Ron <dor.rondel@gmail.com>
2018-02-10 16:13:32 -0500

    updated description & resume

--- dev/js/components/About.js
+++ dev/js/components/About.js
@@ -3,32 +3,38 @@ import React from 'react';
 export default class About extends React.Component {
   render() {
     return (
-      <section id='about'>
+      <section id="about">
         <h1 className="sectionTitle"><u>About</u></h1>
         <div className="row">
           <div className="paragraph col s8 offset-s2">
-            <p> ¡Hola! My name is Dor Rondel and I'm <a href="http://judaism.about.com/od/judaismbasics/g/chai.htm">18</a> years old.
-                I recently completed my freshmen year at the <a href="https://www.ccny.cuny.edu/engineering/">Grove School of Engineering</a>
-                &nbsp;at the <a href="https://www.ccny.cuny.edu/">City College of New York</a> where I plan on majoring
-                in computer science and minoring in biology. Over the past year at CCNY I completed a variety of courses from a
-                diverse <a href="https://www.ccny.cuny.edu/sites/default/files/2015-2016%20Curriculum%20CSc%202015-12-15%20080416.pdf">curriculum</a>,
-                as well as participated in my university's chapter of the <a href="http://www.acm.org/">Association for Computing Machinery</a>.
+            <p> 
+                ¡Hola! My name is Dor Rondel, and I am currently a 19 year old junior planning on
+                getting my <a href="http://catalog.oswego.edu/preview_program.php?catoid=34&poid=3919">
+                computer science BA</a> and cognitive science minor from <a href="https://www.oswego.edu">
+                SUNY Oswego</a> by May 2019. 
             </p>
-            <p> I spend most of my free time doing activities pertaining to programming. I'm particularly interested in <a href="https://generalassemb.ly/blog/what-is-front-end-web-development">front-end web development</a>,
-                ranging from the design aspects to the new specifications of <a href="http://es6-features.org/">ES2015</a>
-                &nbsp;(and '16); such as destructuring and block-scoping, but more importantly, the <a href="http://www.2ality.com/2016/02/exponentiation-operator.html">
-                exponentiaion operator</a> (*chuckle*). I'm currently attempting to master <a href="https://facebook.github.io/react/">React</a> (and I'm loving it - this site was made using it!), learning different
-                software architectures such as: <a href="https://facebook.github.io/flux/">Flux</a>, <a href="http://redux.js.org/">Redux</a>,
-                and <a href="https://mobxjs.github.io/mobx/">Mobx</a>, and how to implement them into React-based applications.
+            <p>
+                I've had a natural affinity towards programming since a young age, and spend most of 
+                my free time programming all sort of different <a href="https://github.com/Dor-Ron">
+                scripts</a> in the following areas: 
+                full-stack web development, bioinformatics, artificial neural networks, 
+                cognitive processes modelling, <a href="https://snips.ai/content/intro-to-ai/#nlp">
+                natural language processing</a>, cryptography, 
+                steganography, and DIY robotics.
             </p>
-            <p> Aside from front-end development, I enjoy learning about other programming fields. Occassionally, I try to do problems do computational
-                biology problems on <a href="http://rosalind.info/users/Dor-Ron/">Project Rosalind</a>. Additionally, I am also a member of <a href="https://ctftime.org/team/20748">CCNYSec</a>,
-                where I compete with my awesome teammates in <a href="https://ctftime.org/ctf-wtf/">CTF</a> competitions to explore my computer security interest. Finally,
-                to complement my interest in front-end development, I dabble with <a href="https://generalassemb.ly/blog/what-is-back-end-web-development/">back-end development</a> as well from time to time.
+            <p>
+                I devoted the most amount of time towards <a href="https://generalassemb.ly/blog/what-is-front-end-web-development/">
+                front-end web development</a>, and am currently
+                learning <a href="https://vuejs.org">VueJS</a>, which is by far my favorite Javascript 
+                UI framework. After Vue, I plan on tackling either React Native or 
+                <a href="https://www.nativescript.org"> Nativescript</a>, PassportJS, and more 
+                AI related projects.
             </p>
-            <p> When I'm not programming, you'd usually find me playing beach volleyball, jogging, or listening to either <a href="https://www.youtube.com/watch?v=I7SdSKKMPNY">foreign rap</a> or <a href="https://www.youtube.com/watch?v=OEzhsAzdRGY">reggaeton</a>.
-                Of course I watch some TV as well, currently I'm watching <a href="http://www.usanetwork.com/mrrobot">Mr. Robot</a> and <a href="http://www.mako.co.il/tv-milers-crossroads?Partner=NavBar">צומת מילר</a>
-                &nbsp;(pronounced Tzomet Meelehr in case you were wondering).
+            <p> 
+                While you'd normally find me programming, on rare occasions I've been spotted doing other 
+                things, such as: playing basketball (go Celtics!), making <a href="https://www.youtube.com/channel/UC9VsutLvgtiQY7thtAUwGUg">
+                tutorial</a> videos (they're in Hebrew), cycling, translating Open Source projects, 
+                and attempting to create <a href="http://artfucksme.com/moki-mioke/">surreal</a> and abstract art. 
             </p>
           </div>
         </div>
