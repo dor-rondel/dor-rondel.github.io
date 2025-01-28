bfa64639a4423b950f83a59bcfc39f9d5e2ad94d
d-0-r <dor.rondel@gmail.com>
2025-01-28 18:30:16 +0200

    fixed avatar head disappearing onZoom

--- src/scenes/Avatar.tsx
+++ src/scenes/Avatar.tsx
@@ -53,6 +53,7 @@ export function Avatar({ animation, wireframe }: AvatarProps) {
           skeleton={nodes.EyeLeft.skeleton}
           morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
           morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
+          frustumCulled={false}
         />
         <skinnedMesh
           name='EyeRight'
@@ -61,6 +62,7 @@ export function Avatar({ animation, wireframe }: AvatarProps) {
           skeleton={nodes.EyeRight.skeleton}
           morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
           morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
+          frustumCulled={false}
         />
         <skinnedMesh
           name='Wolf3D_Head'
@@ -69,6 +71,7 @@ export function Avatar({ animation, wireframe }: AvatarProps) {
           skeleton={nodes.Wolf3D_Head.skeleton}
           morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
           morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
+          frustumCulled={false}
         />
         <skinnedMesh
           name='Wolf3D_Teeth'
@@ -77,31 +80,37 @@ export function Avatar({ animation, wireframe }: AvatarProps) {
           skeleton={nodes.Wolf3D_Teeth.skeleton}
           morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
           morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
+          frustumCulled={false}
         />
         <skinnedMesh
           geometry={nodes.Wolf3D_Hair.geometry}
           material={materials.Wolf3D_Hair}
           skeleton={nodes.Wolf3D_Hair.skeleton}
+          frustumCulled={false}
         />
         <skinnedMesh
           geometry={nodes.Wolf3D_Outfit_Top.geometry}
           material={materials.Wolf3D_Outfit_Top}
           skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
+          frustumCulled={false}
         />
         <skinnedMesh
           geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
           material={materials["Wolf3D_Outfit_Bottom.002"]}
           skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
+          frustumCulled={false}
         />
         <skinnedMesh
           geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
           material={materials.Wolf3D_Outfit_Footwear}
           skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
+          frustumCulled={false}
         />
         <skinnedMesh
           geometry={nodes.Wolf3D_Body.geometry}
           material={materials.Wolf3D_Body}
           skeleton={nodes.Wolf3D_Body.skeleton}
+          frustumCulled={false}
         />
       </group>
     </group>
