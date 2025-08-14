a1cac622c80dd7c24f965b63e54e430c46174be1
dor-rondel <dor.rondel@gmail.com>
2025-08-14 15:26:23 +0300

    minor css changes

--- src/components/Room/IntroductionScene.tsx
+++ src/components/Room/IntroductionScene.tsx
@@ -6,8 +6,8 @@ const IntroductionScene = ({ setScene }: SceneChildProps) => (
   <Scene mobileTop>
     <h1 className='text-4xl md:text-6xl font-extrabold leading-snug mt-8 md:mt-0'>
       Hi, I'm
-      <br className="md:leading-relaxed"/>
-      <span className='bg-white px-1 italic'>Dor Rondel</span>
+      <br />
+      <span className='bg-white px-1 italic md:leading-relaxed'>Dor Rondel</span>
     </h1>
     <motion.p
       className='text-lg text-gray-600 mt-4'
@@ -25,13 +25,13 @@ const IntroductionScene = ({ setScene }: SceneChildProps) => (
       }}
     >
       A software engineer exploring the intersection&nbsp;
-      {window.innerWidth > 600 && <br />}
+      <br className='hidden custom-show-md' />
       between 3D experiences, LLMs & Blockchain
     </motion.p>
     <motion.button
       onClick={() => setScene(3)}
       className={`bg-indigo-600 text-white py-4 px-8 
-        rounded-lg font-bold text-lg mt-4 md:mt-16`}
+        rounded-lg font-bold text-lg mt-4 top-spacing`}
       initial={{
         opacity: 0,
         y: 25,
